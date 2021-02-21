693d83cabe869495bb9e836eaa121237ed07aa39
NitishMakkar <68640209+NitishMakkar@users.noreply.github.com>
2021-02-21 22:15:30 +0530

    Add files via upload

---
+++ BrokenCalculator.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int brokenCalc(int X, int Y) {
+        if(X>=Y){
+            return X-Y;
+        }
+        
+        if(Y%2==0){
+            return 1+brokenCalc(X,Y/2);
+        }
+        else{
+            return 1+brokenCalc(X,Y+1);
+        }
+    }
+}
